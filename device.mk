c0735669db599acd2a057da71a8890d1bc534ff6
Erfan Abdi <erfangplus@gmail.com>
2021-04-28 03:54:57 +0200

    A5_Pro: Cleanup rootdir scripts
    Change-Id: Icc33a6561e43fb699f9f80b455be9f20bc9db385

--- device.mk
+++ device.mk
@@ -165,21 +165,11 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6763.rc \
     init.mt6763.usb.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
     fstab.mt6763 \
     ueventd.mt6763.rc
 
