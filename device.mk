df2e6682721f2ef5199a99783b9c63d99d3e2092
Debayan <kardebayan3@gmail.com>
2021-08-26 11:07:53 +0530

    A5_Pro: Import rootdir scripts from stock
    * From UMIDIGI/A5_Pro/A5_Pro:9/PPR1.180610.011/1616484719:user/release-keys
    
    Change-Id: I101c82acafa642e5efc291ffbd9a2d0cd6a1d12d
    Signed-off-by: Debayan <kardebayan3@gmail.com>

---
+++ device.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6763.rc \
+    init.mt6763.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    multi_init.rc \
+    fstab.mt6763 \
+    ueventd.mt6763.rc
