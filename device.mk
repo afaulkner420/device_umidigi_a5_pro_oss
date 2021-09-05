8fc01dc7ef08a7a82ef60e62745b97ba5a13885a
Debayan Kar <kardebayan3@gmail.com>
2021-09-06 00:58:00 +0530

    A5_Pro: Build android.hardware.radio@1.2
    * Fixes :-
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/mtkfusionrild": library "android.hardware.radio@1.2.so" not found: needed by /vendor/lib64/librilfusion.so in namespace (default)
    
    Change-Id: I1fdca6195438ba691a017f0034162d70f375c570
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     factory_init.connectivity.rc \
