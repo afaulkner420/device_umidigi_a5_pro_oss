16587eb156b88aac7a73b26f9bc9a5137c737a92
Hadenix <vsegda_online_007@bk.ru>
2021-11-08 23:36:14 +0300

    A5_Pro: remove unused libs, fix the platform of some files
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- extract-files.sh
+++ extract-files.sh
@@ -74,7 +74,7 @@ function blob_fixup() {
         vendor/lib/hw/android.hardware.audio@4.0-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
             ;;
-        vendor/lib/hw/audio.primary.mt6771.so)
+        vendor/lib/hw/audio.primary.mt6763.so)
             "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v28.so" "${2}"
             "${PATCHELF}" --replace-needed "libxml2.so" "libxml2-v28.so" "${2}"
             ;;
